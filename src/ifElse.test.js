d8751def3956b5c2194f7cac5367d0808e0fd024
Kate Bass <katebass@MacBook-Pro-Dmitry.local>
2023-02-07 15:34:04 +0200

    Added tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,50 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should be run the second callback', () => {
+  const first = jest.fn();
+  const second = jest.fn();
 
+  ifElse(() => Math.sqrt(25) > 10, first, second);
+  expect(second.mock.calls.length)
+    .toBe(1);
   });
 
-  // write tests here
+  it('should not be run the first callback', () => {
+    const first = jest.fn();
+    const second = jest.fn();
+  
+    ifElse(() => Math.sqrt(25) > 10, first, second);
+    expect(first.mock.calls.length)
+      .toBe(0);
+    });
+
+  it('should be run the first callback', () => {
+    const first = jest.fn();
+    const second = jest.fn();
+    
+    ifElse(() => Math.sqrt(25) > 1, first, second);
+    expect(first.mock.calls.length)
+      .toBe(1);
+    });
+
+  it('should be run the second callback', () => {
+    const first = jest.fn();
+    const second = jest.fn();
+      
+    ifElse(() => Math.sqrt(25) > 5, first, second);
+    expect(second.mock.calls.length)
+      .toBe(1);
+    });
+    
+  it('should not be run the second callback', () => {
+    const first = jest.fn();
+    const second = jest.fn();
+        
+    ifElse(() => Math.sqrt(25) > 1, first, second);
+    expect(second.mock.calls.length)
+     .toBe(0);
+    });  
 });
